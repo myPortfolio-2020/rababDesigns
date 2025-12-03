63fc0889133c5c608b477b2be42197440c276ac2
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-12-03 15:52:46 -0700

    ddf

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // no change
-  experimental: { appDir: true } as any,
 };
 
 export default nextConfig;
