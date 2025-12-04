597300de675c48885ec81ef85bdd7eba38ac5967
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-12-04 01:02:11 -0700

    git

--- app/lib/types/projects.ts
+++ app/lib/types/projects.ts
@@ -1,8 +1,52 @@
+// export interface Project {
+//   id: string;
+//   title: string;
+//   description: string;
+//   myRole: string;
+//   timeline: Timeline;
+//   timelineNotes: string[];
+//   tools: string[];
+//   problems: string[];
+//   goal: string;
+//   persona: Persona;
+//   wireframes: string[];
+//   styleGuide: StyleGuide;
+//   finalDesign: string[];
+//   sketchImages: string[];
+// }
+
+// export interface Timeline {
+//   start: string; // YYYY-MM
+//   end: string; // YYYY-MM
+// }
+
+// export interface Persona {
+//   name: string;
+//   age: number;
+//   occupation: string;
+//   painPoints: string[];
+//   needs: string[];
+// }
+
+// export interface StyleGuide {
+//   colors: string[];
+//   typography: Typography;
+//   components: string[];
+// }
+
+// export interface Typography {
+//   heading: string;
+//   body: string;
+// }
+
+// export interface ProjectsData {
+//   projects: Project[];
+// }
 export interface Project {
   id: string;
   title: string;
   description: string;
-  myRole: string;
+  myRole: string[];
   timeline: Timeline;
   timelineNotes: string[];
   tools: string[];
@@ -29,7 +73,10 @@ export interface Persona {
 }
 
 export interface StyleGuide {
-  colors: string[];
+  colors: {
+    primary: string[];
+    secondary: string[];
+  };
   typography: Typography;
   components: string[];
 }
