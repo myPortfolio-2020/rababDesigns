deda17d9b9908e553023953d52332d279c101625
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-12-08 17:55:58 -0700

    fhgf

--- app/myWork/[workId]/page.tsx
+++ app/myWork/[workId]/page.tsx
@@ -222,45 +222,6 @@ const page = async ({ params }: { params: Promise<{ workId?: string }> }) => {
             </div>
             <div className="flex items-center">
               <div className="text-sm uppercase tracking-[12px] text-[var(--terminalBlue)]">
-                Tools
-              </div>
-              <Image
-                src="/assets/downArrow2.png"
-                alt=""
-                width={70}
-                height={28}
-              />
-            </div>
-          </div>
-          <div className="w-full xl:w-2/3 flex justify-center">
-            <div className="leading-[27px] bracketBox">
-              <div className="flex gap-14">
-                {tools?.map((tool, index) => {
-                  return (
-                    <div key={index}>
-                      <Image
-                        src="/assets/indentArrowBlue.png"
-                        alt="dot"
-                        width={7}
-                        height={9}
-                        className="inline-block mr-2 mb-1"
-                      />
-                      {tool}
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="xl:h-screen xl:w-[80%] h-auto w-full xl:mx-auto flex xl:flex-row flex-col items-center gap-30">
-          <div className="w-full xl:w-1/3">
-            <div className="w-[52px] h-[52px] rounded-full bg-[var(--terminalBlue)] mb-10 flex justify-center items-center">
-              6
-            </div>
-            <div className="flex items-center">
-              <div className="text-sm uppercase tracking-[12px] text-[var(--terminalBlue)]">
                 Persona
               </div>
               <Image
@@ -334,6 +295,45 @@ const page = async ({ params }: { params: Promise<{ workId?: string }> }) => {
         <div className="xl:h-screen xl:w-[80%] h-auto w-full xl:mx-auto flex xl:flex-row flex-col items-center gap-30">
           <div className="w-full xl:w-1/3">
             <div className="w-[52px] h-[52px] rounded-full bg-[var(--terminalBlue)] mb-10 flex justify-center items-center">
+              6
+            </div>
+            <div className="flex items-center">
+              <div className="text-sm uppercase tracking-[12px] text-[var(--terminalBlue)]">
+                Tools
+              </div>
+              <Image
+                src="/assets/downArrow2.png"
+                alt=""
+                width={70}
+                height={28}
+              />
+            </div>
+          </div>
+          <div className="w-full xl:w-2/3 flex justify-center">
+            <div className="leading-[27px] bracketBox">
+              <div className="flex gap-14">
+                {tools?.map((tool, index) => {
+                  return (
+                    <div key={index}>
+                      <Image
+                        src="/assets/indentArrowBlue.png"
+                        alt="dot"
+                        width={7}
+                        height={9}
+                        className="inline-block mr-2 mb-1"
+                      />
+                      {tool}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:h-screen xl:w-[80%] h-auto w-full xl:mx-auto flex xl:flex-row flex-col items-center gap-30">
+          <div className="w-full xl:w-1/3">
+            <div className="w-[52px] h-[52px] rounded-full bg-[var(--terminalBlue)] mb-10 flex justify-center items-center">
               7
             </div>
             <div className="flex items-center">
@@ -348,37 +348,51 @@ const page = async ({ params }: { params: Promise<{ workId?: string }> }) => {
               />
             </div>
           </div>
-          <div className="w-full xl:w-2/3 flex xl:flex-row flex-col justify-center  gap-6">
-            {wireframes?.map((wireframe, index) => {
-              return (
-                <div
-                  key={index}
-                  className="w-full relative border border-[var(--supportingColorBlue)] p-3 min-width-[469px]  transition-transform duration-300 hover:scale-106 hover:z-20"
-                >
-                  <Image
-                    src={wireframe}
-                    alt=""
-                    width={1940}
-                    height={1767}
-                    className="w-full h-auto object-cover"
-                  />
-                </div>
-              );
-            })}
+          <div className="w-full xl:w-2/3">
+            <div className="flex flex-col xl:flex-row xl:flex-wrap gap-8">
+              {wireframes?.map((wireframe, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="w-full relative h-[auto] xl:w-[46%] border border-[var(--supportingColorBlue)] p-3  transition-transform duration-300 hover:scale-106 hover:z-20"
+                  >
+                    <Image
+                      src={wireframe}
+                      alt=""
+                      width={1940}
+                      height={1767}
+                      className="w-full h-auto object-cover"
+                    />
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
 
-        <div className="mt-10 mb-10">
-          <div className="mb-10  bg-[var(--supportingColorBlue)] rounded w-max p-2">
-            Style Guides
-          </div>
-          <div className="flex xl:flex-row flex-col flex-wrap gap-20 mt-6">
-            <div className="w-[200px] text-[var(--terminalBlue)]">
-              Primary Colors
+        <div className="xl:h-screen xl:w-[80%] h-auto w-full xl:mx-auto flex xl:flex-row flex-col items-center gap-30">
+          <div className="w-full xl:w-1/3">
+            <div className="w-[52px] h-[52px] rounded-full bg-[var(--terminalBlue)] mb-10 flex justify-center items-center">
+              8
             </div>
-
-            <div className="w-[130px]">
-              <div className="flex justify-between items-center gap-12">
+            <div className="flex items-center">
+              <div className="text-sm uppercase tracking-[12px] text-[var(--terminalBlue)]">
+                Style guide
+              </div>
+              <Image
+                src="/assets/downArrow2.png"
+                alt=""
+                width={70}
+                height={28}
+              />
+            </div>
+          </div>
+          <div className="w-full xl:w-2/3">
+            <div>
+              <div className="pb-3 text-sm uppercase tracking-[12px] text-[var(--foreground)]">
+                Primary Colors
+              </div>
+              <div className="flex  items-center gap-12">
                 {styleGuide?.colors.primary.map((color, index) => {
                   return (
                     <div
@@ -395,45 +409,41 @@ const page = async ({ params }: { params: Promise<{ workId?: string }> }) => {
                   );
                 })}
               </div>
-            </div>
-          </div>
-          <div className="flex  xl:flex-row flex-col flex-wrap gap-20 mt-6">
-            <div className="w-[200px] text-[var(--terminalBlue)]">
-              Supporting Colors
-            </div>
-
-            <div className="w-[130px]">
-              <div className="flex xl:flex-row flex-col justify-between items-center gap-12">
-                {styleGuide?.colors.secondary.map((color, index) => {
-                  return (
-                    <div
-                      key={index}
-                      style={{ color: color }}
-                      className="flex items-center gap-6"
-                    >
-                      <div> {color}</div>
+              <div className="mt-24">
+                <div className="pb-3 text-sm uppercase tracking-[12px] text-[var(--foreground)]">
+                  Supporting Colors
+                </div>
+                <div className="flex xl:flex-row flex-col justify-between items-center gap-12">
+                  {styleGuide?.colors.secondary.map((color, index) => {
+                    return (
                       <div
-                        className="w-[16px] h-[16px] rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[2px] rounded-br-[20px]"
-                        style={{ backgroundColor: color }}
-                      ></div>
-                    </div>
-                  );
-                })}
+                        key={index}
+                        style={{ color: color }}
+                        className="flex items-center gap-6"
+                      >
+                        <div> {color}</div>
+                        <div
+                          className="w-[16px] h-[16px] rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[2px] rounded-br-[20px]"
+                          style={{ backgroundColor: color }}
+                        ></div>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="mt-10 mb-10">
-          <div className="mb-10  bg-[var(--supportingColorBlue)] rounded w-max p-2">
-            Final Screens
-          </div>
+
+        <div className="xl:h-screen xl:w-[80%] h-auto w-full xl:mx-auto">
+          <div className="text-4xl pb-12 text-center">Final Screens</div>
           <div className="flex flex-col xl:flex-row xl:flex-wrap gap-8">
             {finalDesign?.map((design, index) => {
               const { image, caption } = design;
               return (
                 <div
                   key={index}
-                  className="w-full relative h-[auto] xl:w-[20%] border border-[var(--supportingColorBlue)] p-3  transition-transform duration-300 hover:scale-110"
+                  className="w-full relative h-[auto] xl:w-[26%] border border-[var(--supportingColorBlue)] p-3  transition-transform duration-300 hover:scale-110"
                 >
                   <Image
                     src={image}
@@ -448,6 +458,7 @@ const page = async ({ params }: { params: Promise<{ workId?: string }> }) => {
             })}
           </div>
         </div>
+
         <div className="mt-10 mb-10">
           <div className="mb-10  bg-[var(--supportingColorBlue)] rounded w-max p-2">
             Logos
