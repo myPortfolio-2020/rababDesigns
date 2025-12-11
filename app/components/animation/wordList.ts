fa5924dc1175af5bd86a77daee8e77cd30db2054
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-12-11 15:51:05 -0700

    asd

--- app/components/animation/wordList.ts
+++ app/components/animation/wordList.ts
@@ -1,33 +1,3 @@
-// import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-// gsap.registerPlugin(ScrollTrigger);
-
-// export const workList = (element: HTMLElement) => {
-//   const comeInElement = element.querySelector(".comeIn");
-
-//   if (!comeInElement) return;
-
-//   gsap.fromTo(
-//     comeInElement,
-//     { x: "-100%" }, // Start position
-//     {
-//       x: "0%", // End position
-//       ease: "power1.out",
-//       duration: 0.5,
-//       scrollTrigger: {
-//         trigger: comeInElement,
-//         start: "top 100%",
-//         end: "top 50%",
-//         // start: "top center",
-//         // end: "top 50%",
-//         scrub: 1,
-//         // markers: true,
-//       },
-//     }
-//   );
-// };
-
 // workList.ts
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
