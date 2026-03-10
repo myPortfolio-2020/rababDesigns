3411848e38260750b2a9f11988455c5acb9df9c8
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2026-03-10 15:38:22 -0600

    ddd

--- app/components/ui/homeMiddleSection/NewToMap.tsx
+++ app/components/ui/homeMiddleSection/NewToMap.tsx
@@ -13,20 +13,20 @@ const NewToMap = () => {
     <>
       <div
         ref={containerRef}
-        className="newToMap w-[260px] xl:ml-[200px] ml-[38%] xl:pt-39 pt-[120px]"
+        className="newToMap w-[260px] xl:ml-[200px] ml-[38%] xl:pt-49 pt-[120px]"
       >
-        <div className="text-4xl pb-4">
-          <span>Just new</span>
+        <div className="pb-4">
+          <span className="text-xl">Every Journey</span>
           <br />
-          <span>to the map</span>
+          <span className="text-3xl"> Has a Direction</span>
         </div>
-        <div className="text-xl pb-4">
+        {/* <div className="text-xl pb-4">
           Experienced in my field, new to this city — ready to learn its rhythm
           and contribute my voice.
-        </div>
+        </div> */}
         <div className="bracketBox pb-4">
-          A journey of continuous learning, evolving passions, and purposeful
-          impact.
+          I design for the moments that matter most — when a nurse needs
+          clarity, when a patient feels lost, when a system needs to just work.
         </div>
         <div className="animate-bounce">
           <Image src="/assets/downArrow.png" alt="" width={28} height={70} />
